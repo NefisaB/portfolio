fe07c06ca8c8aa564d5441a8f3fd816e0173dceb
NefisaB <nefisa.hc@gmail.com>
2023-03-20 20:02:26 +0100

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return ( 
         <main>
-            <h1>Nefisa Brcaninovic</h1>
-            <p className="basic-info">
-                Hi and welcome to my portfolio.<br/>
-                As this heading above says my name is Nefisa and I am software developer in making.<br/> 
-                I have tried Java, Android and even Python Programming, but finally settled with JavaScript, and everything 
-                that goes with it.<br/>
-                You can find some of my nicer projects <Link to={"/projects"} >HERE</Link>, and everything else I have done in last 
-                6 months, at my <a href="https://github.com/NefisaB" >GITHUB PAGE</a>.
-            </p>
+            <div className="basic-info">
+                <h1>Nefisa Brcaninovic</h1>
+                <p>Hi and welcome to my portfolio.<br/><br/>
+                    My name is Nefisa and I am software developer in making.<br/> <br/>
+                    I have tried Java, Android and even Python Programming, but finally decided for JavaScript, and everything 
+                    that goes with it.<br/><br/>
+                    You can find some of my nicer projects <Link to={"/projects"} >HERE</Link>, and everything else I have done in last 
+                    6 months, at my <a href="https://github.com/NefisaB" >GITHUB PAGE</a>.
+                    </p>
+                </div>
+            <div className="icons">
+                <img src="/icons/html5.svg" alt="html" className="icon"/>
+                <img src="/icons/css3-alt.svg" alt="css" className="icon"/>
+                <img src="/icons/js.svg" alt="javascript" className="icon" />
+                <img src="/icons/react.svg" alt="react" className="icon"/>
+            </div>
         </main>
      );
 }
